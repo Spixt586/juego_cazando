9c368232ea80803d6b7be30cf181fe473c92d980
Spixt586 <ojtipanluisa@gmail.com>
2026-04-09 21:34:59 -0500

    feat: Define the positions of the cat and food and their sizes

--- cazando.js
+++ cazando.js
@@ -1,7 +1,29 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
- 
+
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ALTO_GATO = 40;
+const ANCHO_GATO = 60;
+const ALTO_COMIDA = 20;
+const ANCHO_COMIDA = 20;
+
 function graficarGato(){
-    ctx.fillStyle = "#ffc800"
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4,canvas.height*2/4);
+    gatoX = canvas.height/2;
+    gatoY = canvas.width/2;
+    ctx.fillStyle = "#0b49e8";
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+function graficarComida(){
+    comidaX = canvas.height-20
+    comidaY = canvas.width-20
+    ctx.fillStyle = "#00ff95ea";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+}
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
