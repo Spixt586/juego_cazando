b7695d366c8846fbacc98f13159763e8c51666b1
Spixt586 <ojtipanluisa@gmail.com>
2026-04-12 12:47:33 -0500

    feat: fix cat centering and movement, fix graficarRectangulo, add moverIzquierda

--- cazando.js
+++ cazando.js
@@ -1,42 +1,38 @@
-let canvas=document.getElementById("areaDeJuego");
+let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 //posicion gato
-let gatoX=0;
-let gatoY=0;
-//posicion comida
-let comidaX=0;
-let comidaY=0;
-//Definimos el tamaño del "gato" (rectángulo)
+let gatoX=canvas.width/2;
+let gatoY=canvas.height/2;
+//posicion comida   
+let comidaX =canvas.width-20
+let comidaY=canvas.height-20
+//gato tamaño
 const anchoGato = 80;
 const altoGato = 50;
-//Definimos el tamaño del cuadrado
+//comida tamaño
 const altoComida = 20;
 const anchoComida = 20;  
- 
-function graficarGato() {
-   
- 
-    // Calculamos el centro exacto del canvas
-    // (Ancho del canvas / 2) - (Ancho del objeto / 2)
-    gatoX = (canvas.width / 2) - (anchoGato / 2);
-    gatoY = (canvas.height / 2) - (altoGato / 2);
-    //color del gato
-    let colorG= "#1900ff";
-    graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, colorG);
-}
- 
-function graficarComida() {
-    graficarRectangulo(comidaX,comidaY,anchoComida, altoComida, "#FF0808");
-}
- 
-function iniciarJuego() {
-    // Llamamos a las funciones que ya creaste
+
+function iniciarJuego(){
     graficarGato();
     graficarComida();
 }
  
-function graficarRectangulo(x,y,ancho,alto,color){
-    ctx.fillStyle = color;
-    ctx.fillRect(x, y, ancho, alto);
+function graficarGato(){
+    graficarRectangulo(gatoX, gatoY, anchoGato,altoGato, "#1900ff");
+}
  
+function graficarComida(){
+    graficarRectangulo(comidaX,comidaY,anchoComida, altoComida, "#FF0808");
+}
+
+function moverIzquierda(){
+    gatoX = gatoX - 10;
+    limpiarCanva();
+    graficarGato();
+    graficarComida();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
---
+++ utils.js
@@ -0,0 +1,4 @@
+function graficarRectangulo(x,y,ancho,alto,color){
+    ctx.fillStyle = color;
+    ctx.fillRect(x,y,ancho,alto)
+}
